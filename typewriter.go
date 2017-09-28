5d7123caf6eb12e1921040db2400afd279d812b1
J Delaney <j.delaney.sd@gmail.com>
2017-09-28 12:52:10 -0700

    Support marking the first difference found

--- typewriter.go
+++ typewriter.go
@@ -5,10 +5,20 @@ import (
 	"fmt"
 )
 
+const (
+	colorOff     = "\033[0m"
+	colorDefault = "\033[41m"
+)
+
 type Config struct {
-	ShowLineNumbers          bool
-	MarkFirstDifference      bool
-	MarkFirstLineDifferences bool
+	ShowLineNumbers bool
+
+	// If true, marks the first spot where the two columns diverge.
+	MarkFirstDifference bool
+
+	// The escape sequence to use to mark a difference. If not specified
+	// defaults to a red background.
+	Marking string
 
 	Padding   int
 	Separator string
@@ -16,6 +26,13 @@ type Config struct {
 
 func maxInt(a, b int) int {
 	if a > b {
+		return a
+	}
+	return b
+}
+
+func minInt(a, b int) int {
+	if a < b {
 		return a
 	}
 	return b
@@ -51,6 +68,26 @@ func rightZero(s string, desiredLength int, padChar string) string {
 	return s
 }
 
+func findDifference(s1, s2 string) (index int, found bool) {
+	minLen := minInt(len(s1), len(s2))
+	for i := 0; i < minLen; i++ {
+		c1 := s1[i]
+		c2 := s2[i]
+
+		if c1 != c2 {
+			return i, true
+		}
+	}
+
+	if len(s1) > len(s2) {
+		return len(s2), true
+	} else if len(s2) > len(s1) {
+		return len(s1), true
+	}
+
+	return -1, false
+}
+
 func Run(lines1, lines2 []string, config Config) string {
 	var buf bytes.Buffer
 
@@ -58,11 +95,27 @@ func Run(lines1, lines2 []string, config Config) string {
 
 	padding := rightZero("", config.Padding, " ")
 
+	if config.Marking == "" {
+		config.Marking = colorDefault
+	}
+
+	differenceFound := false
 	for i := 0; i < maxInt(len(lines1), len(lines2)); i++ {
 		s1 := getOrEmpty(lines1, i)
 		s2 := getOrEmpty(lines2, i)
 
-		s1 = rightZero(s1, leftColumnWidth, " ")
+		extraWidth := 0
+		if config.MarkFirstDifference && !differenceFound {
+			diffIndex, found := findDifference(s1, s2)
+			if found {
+				differenceFound = true
+				s1 = s1[:diffIndex] + config.Marking + s1[diffIndex:] + colorOff
+				s2 = s2[:diffIndex] + config.Marking + s2[diffIndex:] + colorOff
+				extraWidth = len(config.Marking) + len(colorOff)
+			}
+		}
+
+		s1 = rightZero(s1, leftColumnWidth+extraWidth, " ")
 
 		buf.WriteString(s1 + padding + config.Separator + s2 + "\n")
 	}
